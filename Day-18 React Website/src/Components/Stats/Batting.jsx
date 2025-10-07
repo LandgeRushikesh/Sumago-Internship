70f2852e8fef21abc39b0be1b5ecd76c41a401aa
LandgeRushikesh <rushilandge12@gmail.com>
2025-10-07 16:25:15 +0530

    feat: implement Batting component with stats display and add TestFormat data

--- Day-18 React Website/src/Components/Stats/Batting.jsx
+++ Day-18 React Website/src/Components/Stats/Batting.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import TestData from "../Stats/BattingStats/TestFormat.json";
 
 function Batting() {
   const location = useLocation();
-
+  const [stats, setStats] = useState([]);
+  const [category, setCategory] = useState("most runs");
+  const [keys, setKeys] = useState([]);
   const format = location.state?.format;
+
+  useEffect(() => {
+    if (format === "test") {
+      if (category === "most runs") {
+        setStats(TestData["Most Runs"]);
+        setKeys(stats.length > 0 && Object.keys(stats[0]));
+      }
+    }
+  }, [format, category]);
+
+  useEffect(() => {
+    stats.length > 0 ? setKeys(Object.keys(stats[0])) : setKeys([]);
+  }, [stats]);
+
   return (
-    <div>
-      <h1>{format}</h1>
+    <div className="mx-10 my-8 w-full ">
+      {
+        <table className="mx-auto w-[80%] bg-white">
+          <caption>Most Runs</caption>
+          <thead>
+            <tr>
+              {stats.length > 0 &&
+                Object.keys(stats[0]).map((title, index) => (
+                  <th
+                    key={index}
+                    className="text-start border-2 border-black px-2 py-3"
+                  >
+                    {title}
+                  </th>
+                ))}
+            </tr>
+          </thead>
+          <tbody>
+            {stats.length > 0 &&
+              stats.map((stat, index) => (
+                <tr key={index}>
+                  {keys.map((key, index) => (
+                    <td key={index} className="border-2 border-black px-2 py-3">
+                      {stat[key]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      }
     </div>
   );
 }
